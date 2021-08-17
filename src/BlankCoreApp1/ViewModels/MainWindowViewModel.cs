75e872b0c94072a86c023439f25f591a8de6c8b7
chong.lu <luchong1999@outlook.com>
2021-08-17 23:00:42 +0800

    first commit

---
+++ src/BlankCoreApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DryIoc;
+using Prism.Mvvm;
+
+namespace BlankCoreApp1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _content="Hello Prism!";
+
+        public string Content
+        {
+            get { return _content="Hello Prism!"; }
+            set { _content = value; }
+        }
+
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
